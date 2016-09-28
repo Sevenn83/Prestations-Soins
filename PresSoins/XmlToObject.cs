41be2e41720ae7e28d4bcb1da145492c2c249457
Sevenn <thomas.s833@gmail.com>
2016-09-28 14:28:16 +0200

    ajout de la classe traitant le xml

---
+++ PresSoins/XmlToObject.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.ObjectModel;
+using classesMetier;
+
+namespace PresSoins
+{
+    internal class XmlToObject
+    {
+        public static Collection<Dossier> XmlToCollectionDossiers()
+        {
+            var dossiers = new Collection<Dossier>();
+
+
+
+            return dossiers;
+        }
+    }
+}
